2af9b7265a41780d2aa99521837891cb3691ab15
"Carlos" <"carlosgq_@hotmail.com">
2022-05-10 20:35:04 +0200

    Completado contrato votaciones

--- VOTACIONES/votaciones.sol
+++ VOTACIONES/votaciones.sol
@@ -40,6 +40,10 @@ contract votacion{
 
     //  Cualquier persona pueda usar esta funcion para presentarse a las elecciones
     function Representar(string memory _nombrePersona, uint _edadPersona, string memory _idPersona) public{
+        
+        bool existeCandidato = EsCandidato(_nombrePersona);
+        require(existeCandidato==false, "Ya existe el candidato");
+        
         // Hash de los datos del candidato
         bytes32 hashCandidato = keccak256(abi.encodePacked(_nombrePersona, _edadPersona, _idPersona));
 
@@ -54,7 +58,7 @@ contract votacion{
         return candidatos;
     }
 
-    function EsCandidato(string memory nombre ) public view returns(bool){
+    function EsCandidato(string memory nombre ) internal view returns(bool){
         uint length = candidatos.length;
         for(uint i = 0; i < length; i++){
             string memory candidato = candidatos[i];
@@ -66,10 +70,88 @@ contract votacion{
         return false;
     }
 
-    function Votar(string memory nombreCandidato) public{
-        bool esCandidato = EsCandidato(nombreCandidato);
-        if (esCandidato){
-            
+    function VotadoPreviamente(address votante) internal view returns(bool){
+        // Calculamos el hash de la persona que ejecuta la funcion
+        bytes32 hashVotante = keccak256(abi.encodePacked(votante));
+        // Comprobamos que el votante no haya votado ya
+        for(uint i = 0; i < votantes.length; i++){
+            //require(votantes[i]!=hashVotante, "Ya has votado previamente");
+            if (votantes[i]==hashVotante){
+                return true;
+            }
         }
+        return false;
     }
+
+    function Votar(string memory nombreCandidato) public{
+        
+        bool votadoPreviamente = VotadoPreviamente(msg.sender);
+        require(votadoPreviamente==false, "Ya has votado previamente");
+
+        
+        bool existeCandidato = EsCandidato(nombreCandidato);
+        require(existeCandidato==true, "No existe el candidado elegido");
+        
+        bytes32 hashVotante = keccak256(abi.encodePacked(msg.sender));
+        votantes.push(hashVotante);
+        // Añadimos un voto al candidato
+        votosCandidato[nombreCandidato]++;
+            
+    }
+
+    function NumeroVotosCandidato(string memory nombreCandidato) public view returns(uint){
+        bool existeCandidato = EsCandidato(nombreCandidato);
+        require(existeCandidato==true, "No existe el candidado elegido");
+        return votosCandidato[nombreCandidato];
+    }
+
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+
+    function VerResultados() public view returns (string memory){
+        string memory resultados = "";
+
+        for (uint i = 0; i < candidatos.length; i++){
+            string memory candidato = candidatos[i];
+            string memory numeroVotos = uint2str(votosCandidato[candidato]);
+
+            resultados = string(abi.encodePacked(resultados, "(", candidato, ", ", numeroVotos, ")"));
+        }
+
+        return resultados;
+    }
+
+    function Ganador() public view returns (string memory){
+        string memory ganador = candidatos[0];
+
+        for (uint i = 1; i < candidatos.length; i++){
+            if (votosCandidato[ganador]<votosCandidato[candidatos[i]]){
+                ganador = candidatos[i];
+            }
+        }
+
+
+        return ganador;
+
+    }
+    
 }
